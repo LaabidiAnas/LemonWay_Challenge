adac876dd7cd2316093abbaa759af2464c5d4e4f
LaabidiAnas <anasbenmongi@gmail.com>
2019-01-20 17:12:25 +0100

    Lemon Way Challenge

---
+++ ConsoleConsumer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ConsoleConsumer.ServiceReference1;
+using System.Net;
+
+namespace ConsoleConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.Service1SoapClient service = new ServiceReference1.Service1SoapClient();
+
+            Console.WriteLine(service.Fibonacci(10));
+            Console.ReadKey();
+        }
+    }
+}
